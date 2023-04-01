577856ff858d6304d2ba5ff0e09c532b107ca2e9
Michael Ford <michaelford85@gmail.com>
2023-04-01 11:58:57 -0500

    changed to hello world

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def welcomeToKodeKloud():
-    return "Welcome to KODEKLOUD!"
+def index():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
