b217d8ec4a8f07cf9dd5ee925f99f4666c22c355
Michael Ford <michaelford85@gmail.com>
2023-04-03 21:59:47 -0500

    added visitCount variable

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+visitCount = 0
+
 @app.route('/')
 def index():
-    return "Hello World!"
+    global visitCount
+    visitCount+=1
+    return "Hello World! " + str(visitCount)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
