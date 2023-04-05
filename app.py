369306bff076e7bffd3dd761fc7ce27ce5eb8bea
Michael Ford <michaelford85@gmail.com>
2023-04-04 22:56:51 -0500

    added redis reference

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
-
-visitCount = 0
+redisDb = Redis(host='redis-db', port=6379)
 
 @app.route('/')
 def index():
-    global visitCount
-    visitCount+=1
-    return "Hello World! " + str(visitCount)
+    redisDb.incr('visitorCount')
+    visitCount = str(redisDb.get('visitorCount'), 'utf-8')
+    return "Hello World! " + visitCount
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
